4feb1b55cc046a1182f91402e4b3f6cdf0ebab0d
ragilgm <ragilmaulana79@gmail.com>
2023-05-30 02:27:48 +0700

    add spring factories

--- src/main/java/com/commonlib/configuration/BaseConfiguration.java
+++ src/main/java/com/commonlib/configuration/BaseConfiguration.java
@@ -2,14 +2,17 @@ package com.commonlib.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("com.commonlib")
 public class BaseConfiguration {
 
     @Bean
     public ObjectMapper objectMapper(){
         return new ObjectMapper();
     }
+
 
 }
--- src/main/java/com/commonlib/controller/TestController.java
+++ src/main/java/com/commonlib/controller/TestController.java
@@ -1,22 +1,23 @@
-package com.commonlib.controller;
-
-import com.commonlib.annotation.LogExecutionTime;
-import com.commonlib.annotation.LogRequestResponse;
-import com.commonlib.annotation.RacingConditionRejection;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/test")
-public class TestController {
-
-    @GetMapping
-    @LogExecutionTime
-    @LogRequestResponse
-    @RacingConditionRejection
-    public String getTest(){
-        return "hello world";
-    }
-
-}
+//package com.commonlib.controller;
+//
+//import com.commonlib.annotation.LogExecutionTime;
+//import com.commonlib.annotation.LogRequestResponse;
+//import com.commonlib.annotation.RacingConditionRejection;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/test")
+//public class TestController {
+//
+//    @GetMapping
+//    @LogExecutionTime
+//    @LogRequestResponse
+//    @RacingConditionRejection
+//    public String getTest(){
+//        return "hello world";
+//    }
+//
+//}
